e6896273249e5ed8b27dc31cee2d7856f8298719
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-04-23 16:50:57 +0800

    Create 1006.cpp

---
+++ c++/1006.cpp
@@ -0,0 +1,38 @@
+#include<map>
+#include<iostream>
+#include<algorithm>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    string s;
+    int x, j;
+    cin>>s;
+    // map<int, string> mp{{""}}
+    if(s.size()==3){
+        x = s[0] - '0';
+        for(j=0; j<x; j++)
+            cout<<"B";
+        x = s[1] - '0';
+        for(j=0; j<x; j++)
+            cout<<"S";
+        x = s[2] - '0';
+        for(j=0; j<x; j++)
+            cout<<j+1;
+    }
+    if(s.size()==2){
+        x = s[0] - '0';
+        for(j=0; j<x; j++)
+            cout<<"S";
+        x = s[1] - '0';
+        for(j=0; j<x; j++)
+            cout<<j+1;
+    }
+    if(s.size()==1){
+        x = s[0] - '0';
+        for(j=0; j<x; j++)
+            cout<<j+1;
+    }
+    return 0;
+}
