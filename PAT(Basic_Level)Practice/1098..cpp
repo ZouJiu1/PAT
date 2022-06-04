b054b63c2701e44428cd9e6dfa0a3c58062103ae
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-06-04 22:37:32 +0800

    Create 1098..cpp

---
+++ PAT(Basic_Level)Practice/1098..cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int n, num, i, j, m, k, sub;
+    cin>>n;
+    vector<int> v9, v0;
+    for(int i=0; i<n; i++){
+        cin>>num;
+        v0.push_back(num);
+    }
+    for(int i=0; i<n; i++){
+        cin>>num;
+        v9.push_back(num);
+    }
+    sort(v0.begin(), v0.end());
+    sort(v9.begin(), v9.end());
+    m = v9[v9.size()-1]; //max
+    k = v0[0]; //min
+    if(m>=k){
+        cout<<"No "<<1;
+        return 0;
+    }
+    sub = k - m;
+    cout<<"Yes "<<sub;
+    return 0;
+}
