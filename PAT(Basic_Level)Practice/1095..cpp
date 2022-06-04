23cbf3ea015d0386bab1bea5957cd0a0d938c56b
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-06-05 00:43:27 +0800

    Update 1095..cpp

--- PAT(Basic_Level)Practice/1095..cpp
+++ PAT(Basic_Level)Practice/1095..cpp
@@ -28,38 +28,38 @@ int main(void){
     for(int i=0; i<n; i++){
         cin>>vec[i].ID>>vec[i].num;
     }
+    int num;
+    string ID;
     for(int i=1; i<=m; i++){
-        student stt;
-        cin>>stt.num>>stt.ID;
+        cin>>num>>ID;
         vector<student> res;
-        cout<<"Case "<<i<<": "<<stt.num<<" "<<stt.ID<<endl;
-        if(stt.num==1){
+        cout<<"Case "<<i<<": "<<num<<" "<<ID<<endl;
+        if(num==1){
             for(int j=0; j<vec.size(); j++){
-                if(vec[j].ID[0]==stt.ID[0]) res.push_back(vec[j]);
+                if(vec[j].ID[0]==ID[0]) res.push_back(vec[j]);
             }
-            sort(res.begin(), res.end(), compare_firstly);
-        }else if(stt.num==2){
+        }else if(num==2){
             int counts=0, sum=0;
             for(int j=0; j<vec.size(); j++){
-                if(vec[j].ID.substr(1, 3)==stt.ID) {
+                if(vec[j].ID.substr(1, 3)==ID) {
                     counts++;
                     sum += vec[j].num;
                 }
             }
             if(counts==0) cout<<"NA"<<endl;
             else cout<<counts<<" "<<sum<<endl;
-        }else if(stt.num==3){
+        }else if(num==3){
             unordered_map<string, int> mp;
             for(int j=0; j<vec.size(); j++){
-                if(vec[j].ID.substr(4, 6)==stt.ID) mp[vec[j].ID.substr(1, 3)] += 1;
+                if(vec[j].ID.substr(4, 6)==ID) mp[vec[j].ID.substr(1, 3)] += 1;
             }
             for(auto it:mp) res.push_back({it.first, it.second}) ;
-            sort(res.begin(), res.end(), compare_firstly);
         }
+        sort(res.begin(), res.end(), compare_firstly);
         for(int j=0; j<res.size(); j++){
             cout<<res[j].ID<<" "<<res[j].num<<endl;
         }
-        if((res.size()==0)&&(stt.num==3||stt.num==1)) cout<<"NA"<<endl;
+        if((res.size()==0)&&(num!=2)) cout<<"NA"<<endl;
     }
     return 0;
 }
