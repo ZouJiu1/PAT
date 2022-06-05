725369fcbc057487ad09fa7fa334ed790399ece0
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-06-05 17:41:39 +0800

    Update 1068..cpp

--- PAT(Basic_Level)Practice/1068..cpp
+++ PAT(Basic_Level)Practice/1068..cpp
@@ -11,17 +11,22 @@ using namespace std;
 int main(void){
     int n, m, num, inputs, TOL;
     cin>>n>>m>>TOL;
-    int pixels[m][n];
-    for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
+    int pixels[m+2][n+2]={0};
+    map<int, int> mp;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
             cin>>inputs;
             pixels[i][j] = inputs;
+            ++mp[inputs];
         }
     }
+    m = m + 2;
+    n = n + 2;
     int x0, x1, x2, y0, y1, y2, cor=0, tmp, tx, ty;
-    for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
             num = pixels[i][j];
+            if(mp[num]!=1) continue;
             x0 = j - 1;
             x1 = j;
             x2 = j + 1;
@@ -29,22 +34,22 @@ int main(void){
             y1 = i;
             y2 = i + 1;
             if(x0<0||y0<0||x2>=n||y2>=m) continue;
-            if((num - pixels[y0][x0])<=TOL) continue;
-            if((num - pixels[y1][x0])<=TOL) continue;
-            if((num - pixels[y2][x0])<=TOL) continue;
-            if((num - pixels[y0][x1])<=TOL) continue;
-            if((num - pixels[y2][x1])<=TOL) continue;
-            if((num - pixels[y0][x2])<=TOL) continue;
-            if((num - pixels[y1][x2])<=TOL) continue;
-            if((num - pixels[y2][x2])<=TOL) continue;
+            if(abs(num - pixels[y0][x0])<=TOL) continue;
+            if(abs(num - pixels[y1][x0])<=TOL) continue;
+            if(abs(num - pixels[y2][x0])<=TOL) continue;
+            if(abs(num - pixels[y0][x1])<=TOL) continue;
+            if(abs(num - pixels[y2][x1])<=TOL) continue;
+            if(abs(num - pixels[y0][x2])<=TOL) continue;
+            if(abs(num - pixels[y1][x2])<=TOL) continue;
+            if(abs(num - pixels[y2][x2])<=TOL) continue;
             cor++;
             if(cor==2){
                 cout<<"Not Unique";
                 return 0;
             }
             tmp = num;
-            tx = j+1;
-            ty = i+1;
+            tx = j;
+            ty = i;
         }
     }
     if(cor==1) printf("(%d, %d): %d", tx, ty, tmp);
