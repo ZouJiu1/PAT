9d6a1cd9745eeb565a37d91d3372fe4a07714fde
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-07-23 10:19:58 +0800

    Update 1039 Course List for Student.cpp

--- PAT(Advanced_Level)Practice/1039 Course List for Student.cpp
+++ PAT(Advanced_Level)Practice/1039 Course List for Student.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<map>
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -12,39 +13,27 @@ int main(void){
     cin>>m>>n;
     string s;
     int max = -999;
+    map<string, vector<int>> mp;
     for(i = 0 ; i < n; i++)
     {
         cin>>mm>>nn;
         for(j = 0; j < nn; j++){
             cin>>s;
-            vec[mm].insert(s);
+            // vec[mm].insert(s);
+            mp[s].push_back(mm);
         }
         if(mm>max) max = mm;
     }
-    vector<string> vs;
+    vector<int> vs;
     for(i = 0; i < m; i++){
         cin>>s;
-        vs.push_back(s);
-    }
-    set<string>::iterator it;
-    set<string> ste;
-    for(i = 0; i<vs.size(); i++){
-        s = vs[i];
-        vector<int> vtr;
-        for(j = 1; j<=max; j++){
-            ste = vec[j];
-            if(ste.size()==0) continue;
-            it = ste.find(s);
-            if(it!=ste.end()){
-                vtr.push_back(j);
-            }
-        }
-        sort(vtr.begin(), vtr.end());
-        cout<<s<<" "<<vtr.size();
-        for(j = 0; j<vtr.size();j++){
+        vs = mp[s];
+        cout<<s<<" "<<vs.size();
+        sort(vs.begin(), vs.end());
+        for(j = 0; j<vs.size();j++){
             if(j==0) cout<<" ";
-            cout<<vtr[j];
-            if(j != (vtr.size() - 1)) cout<<" ";
+            cout<<vs[j];
+            if(j != (vs.size() - 1)) cout<<" ";
         }
         cout<<endl;
     }
