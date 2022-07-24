1f477dd4003ecf0612cbd13a6a25be7db146f7a6
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-07-24 22:29:35 +0800

    Create 1049 Counting Ones.cpp

---
+++ PAT(Advanced_Level)Practice/1049 Counting Ones.cpp
@@ -0,0 +1,24 @@
+
+
+/*
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int i, j, k, m, n, h, mm, nn;
+    cin>>m;
+    mm = 0;
+    string s;
+    for(i=1; i<=m; i++){
+        s += to_string(i);
+    }
+    for(i = 0; i < s.length(); i++){
+        if(s[i]=='1') mm++;
+    }
+    cout<<mm;
+    return 0;
+}
+*/
