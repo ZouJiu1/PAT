8cba893322dc51c91c1ed42ea072ddb17f65c5e6
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-08-28 21:08:38 +0800

    Create 1085 Perfect Sequence.cpp

---
+++ PAT(Advanced_Level)Practice/1085 Perfect Sequence.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    long long i, j, k, m, n, w, x, y;
+    cin>>n>>m;
+    vector<long long> v;
+    for(i = 0; i < n; i++){
+        cin>>k;
+        v.push_back(k);
+    }
+    
+    sort(v.begin(), v.end());
+    x = -9999999;
+    for(i = n-1; i > 0; i--){
+        w = v[i];
+        k = w/m;
+        for(j = 0; j < n; j++){
+            if(i==j) break;
+            if(v[j] < k) continue;
+            if(w<=v[j]*m){
+                y = i - j + 1;
+                if(y>x) x = y;
+                break;
+            }
+        }
+    }
+    if(x>0) cout<<x;
+    else cout<<v[0];
+    return 0;
+}
