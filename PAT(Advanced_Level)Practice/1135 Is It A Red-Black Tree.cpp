a4ff76ed483042e076ff7c9edb1b1b262553027a
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-10-04 22:25:37 +0800

    Create 1135 Is It A Red-Black Tree.cpp

---
+++ PAT(Advanced_Level)Practice/1135 Is It A Red-Black Tree.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<queue>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct nod {
+    struct nod *l=NULL, *r=NULL;
+    int val;
+};
+
+struct nod* generate_redblack(struct nod *root, int val) {
+    if(root==NULL) {
+        root = new(struct nod);
+        root->val = val;
+        return root;
+    }
+    else if(abs(val) <= abs(root->val)) {
+        root->l = generate_redblack(root->l, val);
+    }else if(abs(val) > abs(root->val)) {
+        root->r = generate_redblack(root->r, val);
+    }
+    return root;
+}
+
+set<int> black_st;
+
+void dfs(struct nod *root, int black) {
+    if(root==NULL) return;
+    if(root->l==NULL && root->r==NULL) { //leave
+        if(root->val > 0)
+            black_st.insert(black + 1);
+        else black_st.insert(black);
+    }
+    if(root->val > 0)
+        dfs(root->l, black + 1);
+    else dfs(root->l, black);
+    if(root->val > 0)
+        dfs(root->r, black + 1);
+    else dfs(root->r, black);
+}
+
+int main(void) {
+    int i, j, m, n, k;
+    vector<vector<int>> v1;
+    cin>>n;
+    v1.resize(n);
+    for(i = 0; i < n; i++) {
+        cin>>m;
+        for(j = 0; j < m; j++) {
+            cin>>k;
+            v1[i].push_back(k);
+        }
+    }
+    for(i = 0; i < n; i++) {
+        struct nod *root = NULL;
+        for(j = 0; j < v1[i].size(); j++) {
+            root = generate_redblack(root, v1[i][j]);
+        }
+        if(root->val < 0) {
+            cout<<"No"<<endl;
+            continue;
+        }
+
+        queue<struct nod*> q;
+        q.push(root);
+        int mr = -9;
+        while(q.size() > 0) {
+            struct nod* nd = q.front();
+            dfs(nd, 0);
+            if(black_st.size()!=1) {
+                mr = 9;
+                break;
+            }
+            black_st.clear();
+            if(nd->l!=NULL) q.push(nd->l);
+            if(nd->r!=NULL) q.push(nd->r);
+            if(nd->val < 0) {
+                if(nd->l!=NULL && nd->l->val < 0) {
+                    mr = 9;
+                    break;
+                }
+                if(nd->r!=NULL && nd->r->val <0) {
+                    mr = 9;
+                    break;
+                }
+            }
+            q.pop();
+        }
+        if(mr > 0) {
+            cout<<"No"<<endl;
+            continue;
+        }
+        cout<<"Yes"<<endl;
+    }
+    return 0;
+}
