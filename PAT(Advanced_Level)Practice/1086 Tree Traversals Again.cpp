55803db543df2dacfbe6194aa5c498fb37d91378
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-10-30 15:06:53 +0800

    Update 1086 Tree Traversals Again.cpp

--- PAT(Advanced_Level)Practice/1086 Tree Traversals Again.cpp
+++ PAT(Advanced_Level)Practice/1086 Tree Traversals Again.cpp
@@ -1,3 +1,48 @@
+update
+#include<stack>
+#include<vector>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+//1 2 3 4 5 6
+//3 2 4 1 6 5
+int m, array[60];
+vector<int> in, pre, post;
+unordered_map<int, int> mp, inp;
+void postorder(int root, int start, int end) {
+    if(start > end) return;
+    int i = inp[pre[root]];
+    postorder(root + 1, start, i - 1);
+    postorder(root + i - start + 1, i + 1, end);
+    post.push_back(pre[root]);
+}
+int main(int argc, char **argv) {
+    int i, j, n, k, y, z = 0;
+    cin>>m;
+    stack<int> t;
+    char cr[6];
+    while(~scanf("%s", cr)) {
+        if(strlen(cr)==4){
+            scanf("%d", &k);
+            mp[z] = k;
+            pre.push_back(z);
+            t.push(z++);
+        } else {
+            inp[t.top()] = in.size();
+            in.push_back(t.top());
+            t.pop();
+        }
+    }
+    postorder(0, 0, m - 1);
+    printf("%d", mp[post[0]]);
+    for(i = 1; i < post.size(); i++) printf(" %d", mp[post[i]]);
+    return 0;
+}
+
+
+old before
 /*先产生颗树然后递归*/
 #include<iostream>
 #include<string>
