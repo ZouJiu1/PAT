b8a98a2699f887aab4bbcbf6a8acb9ac4f78fa0c
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-10-30 20:05:09 +0800

    Update 1099 Build A Binary Search Tree.cpp

--- PAT(Advanced_Level)Practice/1099 Build A Binary Search Tree.cpp
+++ PAT(Advanced_Level)Practice/1099 Build A Binary Search Tree.cpp
@@ -1,3 +1,37 @@
+update2
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int m, n, k, z, y, cnt = 0;
+int arr[101];
+vector<int> level[101], v;
+struct rec{int l=0, r=0;};
+rec tm[101] = {0};
+void inorder(int a, int le) {
+    if(tm[a].l!=-1) inorder(tm[a].l, le + 1);
+    level[le].push_back(arr[cnt++]);
+    if(tm[a].r!=-1) inorder(tm[a].r, le + 1);
+}
+int main(int argc, char **argv) {
+    int i, j;
+    cin>>m;
+    rec rc;
+    for(i = 0; i < m; i++) cin>>tm[i].l>>tm[i].r;
+    for(i = 0; i < m; i++) scanf("%d", &arr[i]);
+    sort(arr, arr + m);
+    inorder(0, 0);
+    for(i = 0; i < 101; i++) {
+        for(j = 0; j < level[i].size(); j++) {
+            v.push_back(level[i][j]);
+        }
+    }
+    printf("%d", v[0]);
+    for(i = 1; i < v.size(); i++) printf(" %d", v[i]);
+    return 0;
+}
+
+update
 #include<iostream>
 #include<vector>
 #include<algorithm>
