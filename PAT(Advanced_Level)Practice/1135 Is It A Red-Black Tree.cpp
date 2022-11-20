f666438ec9d97ed4ed35adf6812358bce9bfc3fa
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-11-20 15:47:21 +0800

    Update 1135 Is It A Red-Black Tree.cpp

--- PAT(Advanced_Level)Practice/1135 Is It A Red-Black Tree.cpp
+++ PAT(Advanced_Level)Practice/1135 Is It A Red-Black Tree.cpp
@@ -1,3 +1,5 @@
+old before
+
 #include<iostream>
 #include<vector>
 #include<set>
@@ -110,6 +112,59 @@ int main(void) {
             continue;
         }
         cout<<"Yes"<<endl;
+    }
+    return 0;
+}
+
+update
+
+#include<iostream>
+#include<queue>
+using namespace std;
+int m, n, ll[31], status;
+struct nod{nod *l=NULL, *r=NULL; int val;};
+nod *generate(nod *root, int val) {
+    if(root==NULL) {
+        root = new(nod);
+        root->val = val;
+    } else if(abs(val) < abs(root->val)) root->l = generate(root->l, val);
+    else if(abs(val) >= abs(root->val)) root->r = generate(root->r, val);
+    return root;
+}
+int getnum(nod *root) {
+    if(root==NULL) return 0;
+    int l = getnum(root->l);
+    int r = getnum(root->r);
+    return root->val > 0? max(l, r) + 1 : max(l, r);
+}
+bool recursion(nod *root) {
+    if(root==NULL) return true;
+    int l = getnum(root->l);
+    int r = getnum(root->r);
+    if(l!=r) return false;
+    return recursion(root->l) && recursion(root->r);
+}
+bool black(nod *root) {
+    if(root==NULL) return true;
+    if(root->val < 0) {
+        if(root->l!=NULL && root->l->val < 0) return false;
+        if(root->r!=NULL && root->r->val < 0) return false;
+    }
+    return black(root->l) && black(root->r);
+}
+int main(int argc, char **argv) {
+    int i, j, k;
+    cin>>m;
+    for(i = 0; i < m; i++) {
+        cin>>n;
+        nod *root = NULL;
+        for(j = 0; j < n; j++) {
+            scanf("%d", &ll[j]);
+            root = generate(root, ll[j]);
+        }
+        if(root->val < 0 ||recursion(root)==false || black(root)==false) {
+            cout<<"No"<<endl;
+        } else cout<<"Yes"<<endl;
     }
     return 0;
 }
