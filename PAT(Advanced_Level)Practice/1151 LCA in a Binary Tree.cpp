6bce037539adfb0e40bf82d21cfdaaed72a26489
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2022-11-20 17:46:59 +0800

    Update 1151 LCA in a Binary Tree.cpp

--- PAT(Advanced_Level)Practice/1151 LCA in a Binary Tree.cpp
+++ PAT(Advanced_Level)Practice/1151 LCA in a Binary Tree.cpp
@@ -1,3 +1,5 @@
+old before 
+
 #include<iostream>
 #include<vector>
 #include<unordered_map>
@@ -48,4 +50,56 @@ int main(void) {
         else lca(0);
     }
     return 0;
+}
+
+update
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int w, m, n, y, z, in[10001], pre[10001], flags, res;
+unordered_map<int, int> mp;
+void lca(int start, int l, int r) {
+    if(l > r || flags > 0 ) return;
+    w = mp[pre[start]];
+    if(mp[y] < w && mp[z] < w) lca(start + 1, l, w - 1);
+    else if(mp[y] >w && mp[z] >w) lca(start + (w - l) + 1, w + 1, r);
+    else if((mp[y] > w && mp[z] < w)||(mp[y] < w && mp[z] > w)){
+        flags = 9;
+        printf("LCA of %d and %d is %d.\n", y, z, pre[start]); 
+    }else if(w==mp[y]) {
+        flags = 9;
+        printf("%d is an ancestor of %d.\n", y, z);
+    }else if(w==mp[z]) {
+        flags = 9;
+        printf("%d is an ancestor of %d.\n", z, y);
+    }
+    return;
+}
+
+int main(int argc, char **argv) {
+    int i, j, k, zeros;
+    cin>>m>>n;
+    for(i = 0; i < n; i++) {
+        scanf("%d", &in[i]);
+        mp[in[i]] = i;
+        if(i==0) zeros = in[i];
+    }
+    for(i = 0; i < n; i++) scanf("%d", &pre[i]);
+    for(i = 0; i < m; i++) {
+        cin>>y>>z;
+        flags = -9;
+        if(mp[y]+0==0&&mp[z]+0==0&&y!=zeros&&z!=zeros) {
+            printf("ERROR: %d and %d are not found.\n", y, z);
+            continue;
+        }else if(mp[y]+0==0&&y!=zeros&&(mp[z]>0||z==zeros)) {
+            printf("ERROR: %d is not found.\n", y);
+            continue;
+        }else if(mp[z]+0==0&&z!=zeros&&(mp[y]>0||y==zeros)) {
+            printf("ERROR: %d is not found.\n", z);
+            continue;
+        }
+        lca(0, 0, n - 1);
+    }
+    return EXIT_SUCCESS;
 }
