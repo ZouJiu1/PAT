f35534e54c035089927c4fccd2c341bea6e5a305
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-01 18:32:42 +0800

    Update 1118 Birds in Forest.cpp

--- PAT(Advanced_Level)Practice/1118 Birds in Forest.cpp
+++ PAT(Advanced_Level)Practice/1118 Birds in Forest.cpp
@@ -1,3 +1,4 @@
+old before
 #include<iostream>
 #include<vector>
 #include<set>
@@ -50,6 +51,58 @@ int main(void) {
         cin>>x1>>x2;
         if(findfather(x1) == findfather(x2)) cout<<"Yes"<<endl;
         else cout<<"No"<<endl;
+    }
+    return 0;
+}
+
+update1
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+
+using namespace std;
+int arr[100000];
+
+int findfather(int a) {
+    int tmp = a;
+    while(arr[a]!=a) a = arr[a];
+    arr[tmp] = a;
+    return a;
+}
+void unionjoin(int a, int c) 
+{
+    int aa = findfather(a);
+    int cc = findfather(c);
+    if(aa < cc) arr[cc] = aa;
+    else arr[aa] = cc;
+}
+int main(int argc, char **argv) {
+    int i, j, k, m, n, y, z, maxmax = -999999999, pre = 0;
+    cin>>m;
+    for(i = 0; i < 100000; i++) arr[i] = i;
+    for(i = 0; i < m; i++) {
+        cin>>n;
+        vector<int> v;
+        for(j = 0; j < n; j++) {
+            cin>>y;
+            v.push_back(y);
+            if(maxmax < y) maxmax = y;
+        }
+        sort(v.begin(), v.end());
+        for(j = 0; j < v.size()-1; j++) 
+            unionjoin(v[j], v[j+1]);
+    }
+    y = 0;
+    for(i = 1; i <= maxmax; i++) {
+        if(findfather(i)==i) y++;
+    }
+    printf("%d %d\n", y, maxmax);
+    cin>>n;
+    for(i = 0; i < n; i++) {
+        cin>>y>>z;
+        if(findfather(y)==findfather(z)) printf("Yes\n");
+        else printf("No\n");
     }
     return 0;
 }
