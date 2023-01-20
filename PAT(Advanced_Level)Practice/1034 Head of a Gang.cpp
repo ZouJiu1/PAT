98786a53feb65cef0188bb0c06895d3c9115fe5b
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-20 17:17:27 +0800

    Update 1034 Head of a Gang.cpp

--- PAT(Advanced_Level)Practice/1034 Head of a Gang.cpp
+++ PAT(Advanced_Level)Practice/1034 Head of a Gang.cpp
@@ -1,3 +1,81 @@
+update
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<map>
+#include<string>
+#include<set>
+#include<algorithm>
+using namespace std;
+unordered_map<string, int> ump;
+map<string, int> res;
+unordered_map<int, string> rev;
+struct nod{int y, z;};
+vector<nod> v[1006];
+int status[1006][1006], num_person, tat[1006], allsum[1006], ind, maxmax;
+set<int> st;
+void recursion(int start, int &sum) {
+    tat[start] = 1;
+    st.insert(start);
+    for(int i = 0; i < v[start].size(); i++) {
+        int y = v[start][i].y, tmp;
+        if(status[start][y]==0) {
+            status[start][y] = 1;
+            sum += v[start][i].z;
+            allsum[start] += v[start][i].z;
+            allsum[y] += v[start][i].z;
+            if(allsum[y] >= allsum[start] && allsum[y] > maxmax) {
+                maxmax = allsum[y];
+                ind = y;
+            }else if(allsum[y] <= allsum[start] && allsum[start] > maxmax) {
+                maxmax = allsum[start];
+                ind = start;
+            }
+            recursion(y, sum);
+        }
+    }
+}
+int main(void) {
+    int i, j, k, m, n, x, y, z, N, K, cnt = 1;
+    string t0, t1;
+    cin>>N>>K;
+    nod nd;
+    for(i = 0; i < N; i++) {
+        cin>>t0>>t1>>z;
+        if(ump[t0]==0) {
+            ump[t0] = cnt;
+            rev[cnt++] = t0;
+        }
+        if(ump[t1]==0) {
+            ump[t1] = cnt;
+            rev[cnt++] = t1;
+        }
+        nd.y = ump[t1];
+        nd.z = z;
+        v[ump[t0]].push_back(nd);
+    }
+    fill(status[0], status[0] + 1006 * 1006, 0);
+    fill(tat, tat + 1006, 0);
+    for(i = 1; i <= cnt; i++) {
+        int sum = 0;
+        maxmax = -999999999;
+        st.clear();
+        if(tat[i]==0) {
+            tat[i] = 1;
+            recursion(i, sum);
+            if(sum > K && st.size() > 2) {
+                res[rev[ind]] = st.size();
+            }
+        }
+    }
+    printf("%d\n", res.size());
+    for(map<string, int>::iterator it=res.begin(); it!=res.end(); it++) {
+        cout<<it->first<<" "<<it->second<<endl;
+    }
+    return 0;
+}
+
+old before
 /* 使用set集合进行编号，然后map进行统计加和，最后遍历的时候要注意不要重复加和*/
 #include<iostream>
 #include<vector>
