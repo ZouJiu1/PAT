e0319028b48f0d0ca6ae6fb6ba81ca124db071a7
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-24 10:32:44 +0800

    Create prein.cpp

---
+++ binarytree/prein.cpp
@@ -0,0 +1,96 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+struct nod {
+    int val = -9;
+    nod *l=NULL, *r=NULL;
+};
+int pre[] = {1, 2, 3, 4, 5, 6};
+int in[] = {3, 2, 4, 1, 6, 5};
+void printvec(vector<int> a, string t){
+    cout<<t<<"\t";
+    for(int i=0; i<a.size();i++){
+        cout<<a[i];
+        if(i!=(a.size()-1))
+            cout<<" ";
+    }
+    cout<<endl;
+}
+/*
+       1
+   2         5
+3    4    6
+*/
+
+vector<int> postarr;
+void poster(int root, int start, int end) {
+    if(start > end) return;
+    int k = start;
+    while(pre[root]!=in[k]) k++;
+    if(k > end) printf("can no gen");
+    poster(root+1, start, k-1);
+    poster(root + k - start + 1, k+1, end);
+    postarr.push_back(pre[root]);
+}
+nod* gentree(nod *root, int preroot, int start, int end) {
+    if(start > end) return NULL;
+    if(root==NULL) {
+        root = new(nod);
+        root->val = pre[preroot];
+    }
+    int k = start;
+    while(pre[preroot]!=in[k]) k++;
+    if(k > end) printf("can no gen");
+    root->l = gentree(root->l, preroot+1, start, k-1);
+    root->r = gentree(root->r, preroot + k - start + 1, k+1, end);
+    return root;
+}
+void preorder(nod *root) {
+    if(root==NULL) return;
+    postarr.push_back(root->val);
+    preorder(root->l);
+    preorder(root->r);
+}
+void inorder(nod *root) {
+    if(root==NULL) return;
+    inorder(root->l);
+    postarr.push_back(root->val);
+    inorder(root->r);
+}
+void postorder(nod *root) {
+    if(root==NULL) return;
+    postorder(root->l);
+    postorder(root->r);
+    postarr.push_back(root->val);
+}
+
+// void postorigin(int root, int start, int end) {
+//     if(start > end) 
+//         return ;
+//     int i = start;
+//     while(i < end && in[i] != pre[root]) i++;
+//     post(root + 1, start, i - 1);
+//     post(root + 1 + i - start, i + 1, end);
+//     printf("%d ", pre[root]);
+// }
+int main() {
+    poster(0, 0, 5); //0pre 0inl 5inr
+    printvec(postarr, "generate postarr:");
+
+    nod *root = NULL;
+    root = gentree(root, 0, 0, 5);
+    postarr.clear();
+    preorder(root);
+    printvec(postarr, "preorder:");
+
+    postarr.clear();
+    inorder(root);
+    printvec(postarr, "inorder:");
+
+    postarr.clear();
+    postorder(root);
+    printvec(postarr, "postorder:");
+    return 0;
+}
