782e7e6129f1155fa43bb9b24a72d4270c883934
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-26 20:08:53 +0800

    Update 1158 Telefraud Detection.cpp

--- PAT(Advanced_Level)Practice/1158 Telefraud Detection.cpp
+++ PAT(Advanced_Level)Practice/1158 Telefraud Detection.cpp
@@ -1,3 +1,73 @@
+update202301 用集合存储receiver，map存储recorder；要注意no more than以及 more than的区别，一个是“<=”另一个才是“>”
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+int arr[1006], status[1006];
+int findfather(int a) {
+    int t = a;
+    while(a!=arr[a]) a = arr[a];
+    arr[t] = a;
+    return a;
+}
+void unionjoin(int a, int c) {
+    int A = findfather(a);
+    int C = findfather(c);
+    if(A>=C) arr[A] = C;
+    else arr[C] = A;
+}
+int main(void) {
+    int i, j, k, m, n, N, M, K, ca, re, du;
+    cin>>K>>N>>M;
+    unordered_map<int, int> ump;
+    set<int> v[1006];
+    for(i = 0; i < M; i++) {
+        cin>>ca>>re>>du;
+        ump[ca * 10000 + re] += du;
+        v[ca].insert(re);
+    }
+    for(i = 0; i < 1006; i++) arr[i] = i;
+    vector<int> fau;
+    for(i = 0; i < 1006; i++) {
+        if(v[i].size() == 0) continue;
+        int cnt = 0, num = 0, shorted = 0;
+        for(auto it:v[i]) {
+            if(ump[i*10000 + it] <= 5 && ump[i*10000 + it] > 0) {
+                shorted++;
+                if(ump[it*10000 + i] > 0) cnt++;
+            }
+        }
+        if(5 * cnt <= shorted && shorted > K) fau.push_back(i);
+    }
+    if(fau.size()==0) {
+        printf("None\n");
+        return 0;
+    }
+    sort(fau.begin(), fau.end());
+    for(i = 0; i < fau.size(); i++) {
+        for(j = i + 1; j < fau.size(); j++) {
+            if(ump[fau[i]*10000 + fau[j]] > 0 && ump[fau[j]*10000+fau[i]] > 0) {
+                unionjoin(fau[i], fau[j]);
+            }
+        }
+    }
+    for(i = 0; i < fau.size(); i++) {
+        if(status[fau[i]]==1) continue;
+        printf("%d", fau[i]);
+        status[fau[i]] = 1;
+        for(j = i + 1; j < fau.size(); j++) {
+            if(findfather(fau[i])==findfather(fau[j])) {
+                status[fau[j]] = 1;
+                printf(" %d", fau[j]);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 old before
 #include<iostream>
 #include<vector>
