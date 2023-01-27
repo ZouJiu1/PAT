46923cf1fb2b6a0df672095bff1314e3f67bd90b
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-27 11:47:59 +0800

    Update 1146 Topological Order 相关求关键路径.cpp

--- PAT(Advanced_Level)Practice/1146 Topological Order 相关求关键路径.cpp
+++ PAT(Advanced_Level)Practice/1146 Topological Order 相关求关键路径.cpp
@@ -79,3 +79,61 @@ int main(int argc, char **argv) {
         if(ump[v[i-1]*1000 + v[i]]!=0) printf(" %d", v[i]);
     return EXIT_SUCCESS;
 }
+
+update202301
+#include<iostream>
+#include<vector>
+#include<map>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int status[1006], deg[1006];
+struct nod{
+    int b, e, v;
+};
+int main(void) {
+    int i, j, ij, k, N, M, x, y, z, inf = 999999999;
+    cin>>N>>M;
+    vector<int> tmp, vk, res, earlytime(1006, 0), lasttime(1006, inf);
+    vector<nod> v[1006];
+    nod nd;
+    map<int, int> mp;
+    for(i = 0; i < M; i++) {
+        cin>>nd.b>>nd.e>>nd.v;
+        v[nd.b].push_back(nd);
+        v[nd.e].push_back(nd);
+        mp[nd.b*10000+nd.e] = mp[nd.e*10000+nd.b] = 1;
+        deg[nd.e]++;
+    }
+    while(vk.size()!=N) {
+        for(i = 1; i <= N; i++) {
+            if(status[i]==0&&deg[i]==0) {
+                for(j = 0; j < v[i].size(); j++) {
+                    deg[v[i][j].e]--;
+                    earlytime[v[i][j].e] = max(earlytime[v[i][j].e], earlytime[v[i][j].b] + v[i][j].v);
+                }
+                vk.push_back(i);
+                status[i] = 1;
+            }
+        }
+    }
+    lasttime[N] = earlytime[N];
+    fill(status, status + 1006, 0);
+    for(z = vk.size() - 1; z >= 0; z--) {
+        i = vk[z];
+        for(j = 0; j < v[i].size(); j++) {
+            lasttime[v[i][j].b] = min(lasttime[v[i][j].b], lasttime[v[i][j].e] - v[i][j].v);
+        }
+    }
+    for(i = 1; i <= N; i++) {
+        if(lasttime[i]==earlytime[i]) {
+            res.push_back(i);
+        }
+    }
+    printf("%d", res[0]);
+    for(i = 1; i < res.size(); i++) {
+        if(mp[res[i]*10000+res[i-1]]==0) continue;
+        printf(" %d", res[i]);
+    }
+    return 0;
+}
