66ce21580a88ede92f2caae86ac38f88b72752e0
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-27 09:33:14 +0800

    Update 1134 Vertex Cover.cpp

--- PAT(Advanced_Level)Practice/1134 Vertex Cover.cpp
+++ PAT(Advanced_Level)Practice/1134 Vertex Cover.cpp
@@ -1,3 +1,68 @@
+update202301   set集合查找比循环比对要好，边只要有一个顶点在set集合就可以，一个O(n)一个是O(n^2)
+#include<iostream>
+#include<set>
+#include<algorithm>
+using namespace std;
+int main(void) {
+    int i, j, ij, k, m, n, K, N, M, y, z, res, arr[10006][2], status[10006];
+    cin>>N>>M;
+    for(i = 0; i < M; i++) {
+        scanf("%d %d", &arr[i][0], &arr[i][1]);
+    }
+    cin>>K;
+    for(i = 0; i < K; i++) {
+        cin>>y;
+        fill(status, status + 10006, 0);
+        res = 0;
+        set<int> st;
+        for(j = 0; j < y; j++) {
+            cin>>z;
+            st.insert(z);
+        }
+        for(ij = 0; ij < M; ij++) {  //O(n)
+            if(st.find(arr[ij][0])!=st.end()||
+                st.find(arr[ij][1])!=st.end())
+                res++;
+        }
+        if(res==M) printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
+
+循环会超时的
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(void) {
+    int i, j, ij, k, m, n, K, N, M, y, z, res, arr[10006][2];
+    cin>>N>>M;
+    for(i = 0; i < M; i++) {
+        scanf("%d %d", &arr[i][0], &arr[i][1]);
+    }
+    cin>>K;
+    for(i = 0; i < K; i++) {
+        cin>>y;
+        vector<bool> status(M, false);
+        res = 0;
+        for(j = 0; j < y; j++) { //O(n^2)
+            cin>>z;
+            for(ij = 0; ij < M; ij++) {
+                if(status[ij]==true) continue;
+                if(arr[ij][0]==z||arr[ij][1]==z) {
+                    res++;
+                    status[ij] = true;
+                }
+            }
+        }
+        if(res==M) printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
+
+old before 
 #include<set>
 #include<iostream>
 #include<vector>
