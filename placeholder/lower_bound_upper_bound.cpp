228894959a1aef8653678fd81d05f05ad43094b0
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-28 19:40:35 +0800

    Update lower_bound_upper_bound.cpp

--- placeholder/lower_bound_upper_bound.cpp
+++ placeholder/lower_bound_upper_bound.cpp
@@ -5,6 +5,12 @@ lower_bound: 7 11
 100 60 50 38 31 30 30 11 9 8 7 6 5 2 1
 upper_bound: greater()8 9
 lower_bound: greater()7 11
+
+
+auto it = lower_bound(v.begin(), v.end(), (long long)v[j] * p, greater<long long>());
+加入greater<long long>());，则是<=，或者upper_bound <，此时v从大到小排列
+默认lower_bound >=   upper_bound >    v从小到大排列
+it – v.begin()拿到index
 */
 #include<iostream>
 #include<algorithm>
@@ -21,13 +27,14 @@ void printfvec(int chr[]) {
 }
 int main(void) {
     int len = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, arr+len); //upper_bound  lower_bound         的arr必须从大到小排列
     printfvec(arr);
     int it = upper_bound(arr, arr+len, 11) - arr; //>的第一个值position
     cout<<"upper_bound: "<<it<<" "<<arr[it]<<endl;
     it = lower_bound(arr, arr+len, 11) - arr; //>=的第一个值position
     cout<<"lower_bound: "<<it<<" "<<arr[it]<<endl;
 
-    sort(arr, arr+len, cmp);
+    sort(arr, arr+len, cmp); //加了greater<int>()  upper_bound  lower_bound     的arr必须从小到大排列
     printfvec(arr);
     it = upper_bound(arr, arr+len, 11, greater<int>()) - arr; //<的第一个值position
     cout<<"upper_bound: greater()"<<it<<" "<<arr[it]<<endl;
