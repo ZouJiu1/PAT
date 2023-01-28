dacecece7084aa42c978f233eca9c4c4af035ca5
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-28 20:37:01 +0800

    Update 1029 Median.cpp

--- PAT(Advanced_Level)Practice/1029 Median.cpp
+++ PAT(Advanced_Level)Practice/1029 Median.cpp
@@ -1,3 +1,40 @@
+update202301
+可以直接用sort排序，不过题目的意思应该不是这个，所以用归并的思想，使用双指针，对序列进行排序，
+某个指针指向end，就可以退出while，此时mid的position已经确定了
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(void) {
+    int i, j, k, m, n, N, M, y=0, z=0, res, mid, cnt = 0;
+    cin>>N;
+    vector<int> a0(N);
+    for(i = 0; i < N; i++) scanf("%d", &a0[i]);
+    cin>>M;
+    vector<int> a1(M);
+    for(j = 0; j < M; j++) scanf("%d", &a1[j]);
+    mid = (N + M - 1) / 2;
+    while(y < N && z < M) {
+        if(a0[y] < a1[z]) {
+            res = a0[y];
+            y++;
+        }
+        else {
+            res = a1[z];
+            z++;
+        }
+        if(y + z > mid) break;
+    }
+    if(y < N  && y + z <= mid) {
+        res = a0[mid - z];
+    }
+    else if(z < N && y + z <=mid) {
+        res = a1[mid - y];
+    }
+    cout<<res<<endl;
+    return 0;
+}
+
+old before
 #include<iostream>
 #include<vector>
 #include<bits/stdc++.h>
