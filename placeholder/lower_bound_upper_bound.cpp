95b22c5137ba96c48c619470c907e4b35932045e
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-01-28 20:32:43 +0800

    Update lower_bound_upper_bound.cpp

--- placeholder/lower_bound_upper_bound.cpp
+++ placeholder/lower_bound_upper_bound.cpp
@@ -26,14 +26,14 @@ void printfvec(int chr[]) {
 }
 int main(void) {
     int len = sizeof(arr)/sizeof(arr[0]);
-    sort(arr, arr+len); //upper_bound  lower_bound         的arr必须从大到小排列
+    sort(arr, arr+len); //upper_bound  lower_bound         的arr必须从小到大排列
     printfvec(arr);
     int it = upper_bound(arr, arr+len, 11) - arr; //>的第一个值position
     cout<<"upper_bound: "<<it<<" "<<arr[it]<<endl;
     it = lower_bound(arr, arr+len, 11) - arr; //>=的第一个值position
     cout<<"lower_bound: "<<it<<" "<<arr[it]<<endl;
 
-    sort(arr, arr+len, cmp); //加了greater<int>()  upper_bound  lower_bound     的arr必须从小到大排列
+    sort(arr, arr+len, cmp); //加了greater<int>()  upper_bound  lower_bound     的arr必须从大到小排列
     printfvec(arr);
     it = upper_bound(arr, arr+len, 11, greater<int>()) - arr; //<的第一个值position
     cout<<"upper_bound: greater()"<<it<<" "<<arr[it]<<endl;
