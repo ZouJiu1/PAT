82588d11c5c6e7ce40cd1680af80e443d3f01043
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-02-04 10:55:25 +0800

    Update mergesort.cpp

--- placeholder/mergesort.cpp
+++ placeholder/mergesort.cpp
@@ -1,4 +1,7 @@
 https://blog.csdn.net/m0_50617544/article/details/128761866?csdn_share_tail=%7B%22type%22%3A%22blog%22%2C%22rType%22%3A%22article%22%2C%22rId%22%3A%22128761866%22%2C%22source%22%3A%22m0_50617544%22%7D
+将两个有序的序列合并到一个序列，叫做归并的
+
+归并排序每次归并的长度*2，归并长度从1开始，然后依次是2，2^2, ...，直到归并长度>=数组长度的，就可以退出
 
 #include<iostream>
 #include<vector>
@@ -69,6 +72,38 @@ void mergesort(int len) {
             while(ll < end) tmp.push_back(arr[ll++]);
         }
         arr = tmp;
+    }
+}
+int main(int argc, char **argv) {
+    int len = arr.size();
+    if(len==1) {
+        printvec(len);
+        return EXIT_SUCCESS;
+    }
+    mergesort(len);
+    printvec(len);
+    return EXIT_SUCCESS;
+}
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> arr = {2, 9, 6, 1, 5, 8, 7, 11, 100, 30, 31, 38, 60, 50, 30};
+void printvec(int len) {
+    printf("%d", arr[0]);
+    for(int i = 1; i < len; i++) printf(" %d", arr[i]);
+}
+void mergesort(int len) {
+    int i, j, ij, k = 1, start, end, Khalf;
+    while(true) {
+        k = k * 2;
+        if(k >= len) break;
+        for(i = 0; i < len / k; i++) {
+            sort(arr.begin() + i * k, arr.begin() + (i+1) * k);
+        }
+        if(len%k > 0)sort(arr.begin() + i * k, arr.end());
     }
 }
 int main(int argc, char **argv) {
