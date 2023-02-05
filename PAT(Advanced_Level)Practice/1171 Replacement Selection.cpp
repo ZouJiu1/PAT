9f3a6642c757d326ee3cf4f0625e6d60d871b2a1
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-02-05 15:50:55 +0800

    Create 1171 Replacement Selection.cpp

---
+++ PAT(Advanced_Level)Practice/1171 Replacement Selection.cpp
@@ -0,0 +1,41 @@
+堆优化的队列，priority_queue最开始的那个值就是最小值，产生最小的堆greater<int>
+若进来值<pop出去值，进来值run加一暂时不参加排序，也就是queue数组的index + 1
+  
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+vector<int> v, tp[100];
+int main(void) {
+    int i, j, k, N, M, K, y, z, mr = 9, cnt, run = 0, ww = 0;
+    cin>>N>>M;
+    for(i = 0; i < N; i++) {
+        scanf("%d", &k);
+        v.push_back(k);
+    }
+    priority_queue<int, vector<int>, greater<int>> pq[100];
+    for(i = 0; i < min(M, (int)v.size()); i++) pq[run].push(v[i]);
+    cnt = pq[run].size();
+    while(!pq[run].empty()) {
+        z = pq[run].top();
+        tp[run].push_back(z);
+        pq[run].pop();
+        if(cnt < v.size() && v[cnt] < z) {   //放到下一个run
+            pq[run + 1].push(v[cnt++]);
+        }
+        else if(cnt < v.size()) {
+            pq[run].push(v[cnt++]);
+        }
+        if(pq[run].empty()) {   //当前run全部空以后，run+1
+            run++;
+        }
+    }
+    for(i = 0; i < 100; i++) {
+        for(j = 0; j < tp[i].size(); j++) {
+            printf("%d", tp[i][j]);
+            if(j!=tp[i].size() - 1) printf(" ");
+            else printf("\n");
+        }
+    }
+    return 0;
+}
