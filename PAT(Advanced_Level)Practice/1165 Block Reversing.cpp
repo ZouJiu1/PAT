ee68847270cf74cae21aab63a1c2f07ba5add138
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-02-05 17:06:05 +0800

    Update 1165 Block Reversing.cpp

--- PAT(Advanced_Level)Practice/1165 Block Reversing.cpp
+++ PAT(Advanced_Level)Practice/1165 Block Reversing.cpp
@@ -1,3 +1,4 @@
+old before
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -30,5 +31,47 @@ int main(void) {
     for(i = 0; i < vv.size() - 1; i++) 
         printf("%05d %d %05d\n", vv[i].a, vv[i].d, vv[i+1].a);
     printf("%05d %d -1\n", vv[i].a, vv[i].d);
+    return 0;
+}
+
+update202302
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct nod {int add, val, net; };
+nod arr[100006];
+int main(void) {
+    int i, j, k, m, n, N, M, K, h, y, z, TAT;
+    cin>>TAT>>N>>K;
+    nod nd;
+    for(i = 0; i < N; i++) {
+        cin>>nd.add>>nd.val>>nd.net;
+        arr[nd.add] = nd;
+    }
+    h = TAT;
+    vector<nod> v, res;
+    while(true) {
+        if(h==-1) break;
+        v.push_back(arr[h]);
+        h = arr[h].net;
+    }
+    reverse(v.begin(), v.end());
+    h = v.size()%K;
+    for(i = 0; i < v.size()/K + int(h > 0); i++) {
+        if(i==0 && h > 0) {
+            for(j = h - 1; j>=0; j--) res.push_back(v[j]);
+        }
+        else if(h > 0) {
+            for(j = h + K * i - 1; j >= h + K * (i-1); j--) res.push_back(v[j]);
+        } else {
+            for(j = K * (i+1) - 1; j >= K * i; j--) res.push_back(v[j]);
+        }
+    }
+    for(i = 0; i < res.size() - 1; i++) {
+        printf("%05d %d %05d\n", res[i].add, res[i].val, res[i+1].add);
+    }
+    printf("%05d %d -1\n", res[i].add, res[i].val);
+
     return 0;
 }
