de9d526078fd026ac1ef2a0bebabc3208706289f
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-03-12 10:17:40 +0800

    Create PAT甲级 2023年spring考试 PAT甲级 2023年spring考试 The Winner over Squares and Primes.cpp

---
+++ examination/PAT甲级 2023年spring考试 PAT甲级 2023年spring考试 The Winner over Squares and Primes.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+using namespace std;
+bool isprime(int &a) {
+    if(a <= 1) return false;
+    for(int i = 2; i * i <= a; i++) {
+        if(a%i==0) return false;
+    }
+    return true;
+}
+
+
+
+int main(void) {
+    int i, j, k, m, n, N, M, K, inf = 999999999;
+    cin>>N;
+    vector<int> v, r, rr;
+    for(i = 1; i <= N; i++) v.push_back(i);
+    k = 0;
+    while(v.size()!=1) {
+        r = v;
+        rr.clear();
+        if(k%2==0) {
+            for(i = 1; i <=r.size(); i++) {
+                double kk = sqrt((double)i);
+                K = (int)kk;
+                if(fabs(K - kk) < 0.00000001) {
+                    continue;
+                }else{
+                    rr.push_back(r[i-1]);
+                }
+            }
+        }else{
+            for(i=1; i<=r.size(); i++) {
+                if(isprime(i)){
+                    continue;
+                }else{
+                    rr.push_back(r[i-1]);
+                }
+            }
+        }
+        v = rr;
+        k++;
+    }
+    cout<<v[0]<<endl;
+    return 0;
+}
