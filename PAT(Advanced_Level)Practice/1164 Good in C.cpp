f7dab69ad72e9f70e329fa3bbed463073a9c1fc1
ZouJiu1 <34758215+ZouJiu1@users.noreply.github.com>
2023-10-10 17:00:04 +0800

    Update 1164 Good in C.cpp

--- PAT(Advanced_Level)Practice/1164 Good in C.cpp
+++ PAT(Advanced_Level)Practice/1164 Good in C.cpp
@@ -1,3 +1,43 @@
+update20231010
+减少特殊情况，去掉各类符号，只考虑英文字符
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+string alpha[26][7];
+int main(void) {
+    int i, j, k, m, n, x, y, z;
+    vector<string> c0, c1;
+    for(i = 0 ; i < 26; i++) {
+        for(j = 0; j < 7; j++) {
+            getline(cin, alpha[i][j]); // can not use scanf here
+        }
+    }
+    string in, r0, r1;
+    getline(cin, in);
+    for(i = 0; i < in.length(); i++) {
+        if(isupper(in[i])&&isalpha(in[i])) {
+            r0 += in[i];
+            if(i==in.length() - 1) c0.push_back(r0);
+        } else {
+            if(r0.length() > 0) c0.push_back(r0);
+            r0.clear();
+        }
+    }
+    for(i = 0; i < c0.size(); i++) {
+        for(k = 0; k < 7; k++) {
+            for(j = 0; j < c0[i].length(); j++) {
+                cout<<alpha[c0[i][j]-'A'][k];
+                if(j!=c0[i].length() - 1) cout<<" ";
+            }
+            cout<<endl;
+        }
+        if(i!=c0.size() - 1) cout<<endl;
+    }
+    return 0;
+}
+
 update202302
 不能存在多的空行，测试案例是--H--EL1LO--，空字符串不放入列表，最后一个字符可能是alpha，要特殊处理，
 最后一行要输出\n，\n不是空行是nextline，两个\n\n才是空行的
@@ -88,6 +128,69 @@ int main(void) {
     return 0;
 }
 
+20231010 update
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+char alpha[26][7][6], out[7][100];
+
+int main(void) {
+    int i, j, k, m, n = 0, x, y, z, last;
+    char kk;
+    for(i = 0; i < 26; i++) {
+        for(j = 0; j < 7; j++) {
+            scanf("%s", alpha[i][j]);
+        }
+    }
+    string in, rin;
+    getchar();
+    getline(cin, in);
+    y = -6;
+    rin = in;
+    reverse(rin.begin(), rin.end());
+    for(i = 0; i < rin.length(); i++) {
+         if(rin[i] <= 'Z' && rin[i] >= 'A') {
+            last = rin.length() - i;
+            break;
+         }
+    }
+    for(i = 0; i < in.length(); i++) {
+        if(in[i] >'Z' || in[i] < 'A') {
+            if(y > 0) {
+                for(j = 0; j < 7; j++) {
+                    for(k = 0; k < n-1; k++) {
+                        printf("%c", out[j][k]);
+                    }
+                    printf("\n");
+                }
+                if(i!=in.length() - 1 && i!=last) printf("\n");
+                n = 0;
+            }
+            y = -6;
+            continue;
+        }
+        y = 6;
+        x = in[i] - 'A';
+        for(j = 0; j < 7; j++) {
+            for(k = 0; k < 5; k++) {
+                out[j][n + k] = alpha[x][j][k];
+            }
+            out[j][n + k] = ' ';
+        }
+        n += 6;
+    }
+    if(in[i-1] <= 'Z' && in[i-1] >= 'A' && n > 0) {
+        for(j = 0; j < 7; j++) {
+            for(k = 0; k < n-1; k++) {
+                printf("%c", out[j][k]);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
 
 old before
 #include<iostream>
